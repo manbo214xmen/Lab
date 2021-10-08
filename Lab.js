8328af01762f9e0be796cd7c4c78536c785ad60e
manbo214xmen <manbo214xmen@gmail.com>
2021-10-08 11:30:10 +0700

    LMAO

---
+++ Lab.js
@@ -0,0 +1,55 @@
+const http = require("http");
+var fs = require("fs");
+
+const hostname = "localhost";
+
+const port = 8000;
+var solan = 0;
+
+const server = http.createServer(
+    (req, res) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type","text/html");
+        //res.end("{ 'message' : 'Hello' , 'Num.' : " + solan + "}");
+        console.log("--- request !!!", solan++, req.url);
+        switch (req.url) {
+            case "/home":
+                res.writeHead(200);
+                var data = fs.readFileSync("./bistro.html");
+                res.end(data);
+                break;
+            case "/about":
+                res.writeHead(200);
+                res.end("<h1>This is About page</h1>");
+                break;
+            case "/login":
+                res.writeHead(200);
+                res.end("<h1>This is Login page</h1>");
+                break;
+            case "/logout":
+                res.writeHead(200);
+                res.end("<h1>This is Logout page</h1>");
+                break;
+            case "/contact":
+                res.writeHead(200);
+                res.end("<h1>This is Contact page</h1>");
+                break;
+            case "/order":
+                res.writeHead(200);
+                res.end("<h1>This is Order page</h1>");
+                break;
+            default:
+                res.writeHead(200);
+                res.end("<h1>.....Error!!!</h1>");
+                break;
+        }
+        
+    }
+)
+function XuLyThongTinServerDuocBat() {
+    console.log("Listen !!!" , hostname , port );
+}
+
+server.listen( port, hostname, XuLyThongTinServerDuocBat)
+   
+
